b68d09fbd66913fd8791f939e8074346256d9a60
Emiliano Bivachi <emilianobivachi@gmail.com>
2018-10-13 21:24:08 -0300

    Swift 4.2 (#6)
    * Convert to Swift 4.2
    
    * Change open to public
    
    * Revert "Change open to public"
    
    This reverts commit f2d9624c959da3b8957d66bcd1f832dbee6a4e07.
    
    * Fix

--- PasscodeLockDemo/AppDelegate.swift
+++ PasscodeLockDemo/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return presenter
     }()
     
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         passcodeLockPresenter.presentPasscodeLock()
         
